a511ff3ee19d5165179d53fbb31b38a936bd6c91
Bibin <b.prataph>
2023-09-09 12:52:23 +0400

    commit

---
+++ build/precache-manifest.be789c6b9fc826d7c1ed89800367aa06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba5e1e75d4f010fa4c04359215dd2c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82f1ed729c76fd55b4ba",
+    "url": "/static/css/main.920c4366.chunk.css"
+  },
+  {
+    "revision": "5764603d6fa95484cbfa",
+    "url": "/static/js/2.20e80f41.chunk.js"
+  },
+  {
+    "revision": "a0912341a04ff50bb0de6882508a5c05",
+    "url": "/static/js/2.20e80f41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82f1ed729c76fd55b4ba",
+    "url": "/static/js/main.c62b1395.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  }
+]);
